9733d415f8eb3bfaf9402aff8ab24c6bb1e7e0b8
Archibald <1961395960@qq.com>
2022-05-11 14:06:23 +0800

    调整title文本

--- Server/ServerApp/Public/index.php
+++ Server/ServerApp/Public/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>trans tool</title>
+    <title>tool</title>
     <link rel="stylesheet" href="./vendor/bootstrap.min.css">
     <link rel="stylesheet" href="./public/css/style.css">
     <link rel="stylesheet" href="./public/css/element.css">
